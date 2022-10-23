cf42d1bab1b1943235a5fc5bd42f9be97ccd4482
Elias Menon <eliasmenon@gmail.com>
2022-10-23 15:10:28 -0300

    Add a fix for overlapping subtitles

--- main.go
+++ main.go
@@ -9,15 +9,42 @@ import (
 )
 
 func main() {
-	args := os.Args
-	getopt.Parse()
-	fs, err := os.Open("./test.srt")
+	_, err := getopt.Parse()
 	if err != nil {
 		return
 	}
-	subs, err := srt.Parse(fs)
+
+	inputFile, err := os.Open("./test.srt")
 	if err != nil {
 		return
 	}
-	fmt.Print(subs, args)
+	defer inputFile.Close()
+	subs, err := srt.Parse(inputFile)
+	if err != nil {
+		return
+	}
+	var newSubs []*srt.Subtitle
+	for i := 0; i < len(subs); i++ {
+		sub := &srt.Subtitle{
+			Index:     subs[i].Index,
+			StartTime: subs[i].StartTime,
+			EndTime:   subs[i].EndTime,
+			Dialogue:  subs[i].Dialogue,
+		}
+		for j := i + 1; j < len(subs) && subs[i].StartTime == subs[j].StartTime && subs[i].EndTime == subs[j].EndTime; j++ {
+			sub.Dialogue = append(sub.Dialogue, subs[j].Dialogue...)
+			i = j
+		}
+		newSubs = append(newSubs, sub)
+	}
+
+	outputFile, err := os.Create("./output.srt")
+	if err != nil {
+		return
+	}
+	defer outputFile.Close()
+	err = srt.Save(newSubs, outputFile)
+	if err != nil {
+		fmt.Print("Error\n")
+	}
 }
