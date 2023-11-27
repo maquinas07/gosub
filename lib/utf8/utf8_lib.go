1c39e4417440e33d169e20e6c88e79fda41d7242
Elias Menon <eliasmenon@gmail.com>
2023-11-27 03:20:22 -0300

    Fix UTF8 BOM strip and add test

--- lib/utf8/utf8_lib.go
+++ lib/utf8/utf8_lib.go
@@ -5,8 +5,8 @@ import "encoding/binary"
 var utf8bom uint32 = 0xEFBBBF00
 
 func StripUTF8BOM(data []byte) []byte {
-    if utf8bom & binary.BigEndian.Uint32(data[:4]) == utf8bom {
-        return data[4:]
-    }
-    return data
+	if utf8bom&binary.BigEndian.Uint32(data[:4]) == utf8bom {
+		return data[3:]
+	}
+	return data
 }
